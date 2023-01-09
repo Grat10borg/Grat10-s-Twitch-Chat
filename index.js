96e116a4c0990523482750265df1652a956828dc
Grat <109081987+Grat10borg@users.noreply.github.com>
2023-01-09 09:59:19 +0100

    GratChat clear old chat msgs after 19 chats

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 "use strict";
 var chat = document.querySelector("#chat>ul");
 ComfyJS.onChat = (user, message, flags, self, extra) => {
-    console.log(user, message, flags, self, extra);
+    if (extra["userBadges"]["broadcaster"] == 1 || extra["userBadges"]["mod"] == 1) {
+        CreateChatText(message, user, "none", "none", ["none", "none"]);
+    }
+    else {
+        CreateChatText(message, user, "none", "none", ["none", "none"]);
+    }
+};
+ComfyJS.onCommand = (user, command, message, flags, extra) => {
+    if (flags.broadcaster && command === "test") {
+        console.log("!test was typed in chat");
+    }
+};
+ComfyJS.Init("grat_grot10_berg");
+function CreateChatText(message, user, colour, profilePic, emotes) {
+    var _a;
     var newMessage = document.createElement("li");
     var text = document.createElement("blockquote");
     newMessage.innerText = user;
     text.innerText = message;
     newMessage.append(text);
     chat.append(newMessage);
-};
-ComfyJS.Init("grat_grot10_berg");
+    if (chat.getElementsByTagName("li").length > 19) {
+        (_a = chat.firstElementChild) === null || _a === void 0 ? void 0 : _a.remove();
+    }
+}
--- index.ts
+++ index.ts
@@ -8,13 +8,44 @@ ComfyJS.onChat = (
   self: any,
   extra: any
 ) => {
-  console.log(user, message, flags, self, extra);
-  var newMessage = document.createElement("li");
-  var text = document.createElement("blockquote");
-  newMessage.innerText = user;
-  text.innerText = message;
-  newMessage.append(text);
-  chat.append(newMessage);
+  // console.log(user);
+  // console.log(message);
+  // console.log(flags);
+  // console.log(self);
+  // console.log(extra);
+
+  if (
+    extra["userBadges"]["broadcaster"] == 1 || extra["userBadges"]["mod"] == 1) {
+      // MOD or Broadcaster color adding
+      CreateChatText(message,user, "none", "none", ["none", "none"]);  
+  } 
+  else {
+    // Normal User adding
+    CreateChatText(message,user, "none", "none", ["none", "none"]);
+  }
 };
+
+// Command Handling
+//@ts-expect-error
+ComfyJS.onCommand = ( user:any, command:any, message:any, flags:any, extra:any ) => {
+  if( flags.broadcaster && command === "test" ) {
+    console.log( "!test was typed in chat" );
+  }
+}
 //@ts-expect-error
 ComfyJS.Init("grat_grot10_berg");
+
+function CreateChatText(message:string, user: string, colour: string, profilePic:string, emotes:Array<string>) {
+  var newMessage = document.createElement("li");
+    var text = document.createElement("blockquote");
+    newMessage.innerText = user;
+    text.innerText = message;
+    newMessage.append(text);
+    chat.append(newMessage);
+
+    
+    // removes old chat messages outside of view.
+    if(chat.getElementsByTagName("li").length > 19) {
+      chat.firstElementChild?.remove();
+    }
+}
