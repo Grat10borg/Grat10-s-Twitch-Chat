e76437654bfce0ba853298d8307e1602f3800ae0
Grat <109081987+Grat10borg@users.noreply.github.com>
2023-01-10 13:40:55 +0100

    Madeline has been deleted 😔 (the text image)

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var chat = document.querySelector("#chat>ul");
 let ClearOldChatMSGsAfter = 11;
+let AppAcessToken = "gksx1g5gtd21dukq6t3cdafslng28t";
+let AclientId = "";
+validateToken();
 ComfyJS.onChat = (user, message, flags, self, extra) => {
     console.log(user);
     console.log(message);
@@ -16,9 +28,7 @@ ComfyJS.onChat = (user, message, flags, self, extra) => {
 };
 ComfyJS.onCommand = (user, command, message, flags, extra) => {
     if (command === "lurk") {
-        ComfyJS.Say("Have a nice lurk @" +
-            user +
-            "!! 🌺🌸");
+        ComfyJS.Say("Have a nice lurk @" + user + "!! 🌺🌸");
     }
     if (flags.broadcaster || flags.mod) {
         if (command === "test") {
@@ -33,16 +43,100 @@ ComfyJS.onCommand = (user, command, message, flags, extra) => {
 ComfyJS.onMessageDeleted = (id, extra) => {
     console.log(id, extra);
 };
-ComfyJS.Init("illu_illusion", "", "grat_grot10_berg");
+ComfyJS.Init("illu_illusion", "oauth:ittskpnutx42o6zlmps13yotc9zylg", "grat_grot10_berg");
+var ChatProfiles = Array();
 function CreateChatText(message, user, colour, profilePic, emotes) {
     var _a;
-    var newMessage = document.createElement("li");
-    var text = document.createElement("blockquote");
-    newMessage.innerText = user;
-    text.innerText = message;
-    newMessage.append(text);
-    chat.append(newMessage);
-    if (chat.getElementsByTagName("li").length > ClearOldChatMSGsAfter) {
-        (_a = chat.firstElementChild) === null || _a === void 0 ? void 0 : _a.remove();
-    }
+    return __awaiter(this, void 0, void 0, function* () {
+        let profilePicIMG = document.createElement("img");
+        if (ChatProfiles.indexOf(user) == -1) {
+            let User = yield HttpCalling("https://api.twitch.tv/helix/users?login=" + user);
+            console.log(User);
+            profilePicIMG.src = User["data"][0]["profile_image_url"];
+            ChatProfiles.push(User["data"][0]["login"] + "#" + User["data"][0]["profile_image_url"]);
+        }
+        else {
+            console.log(ChatProfiles);
+            profilePicIMG.src = ChatProfiles[ChatProfiles.indexOf(user)];
+        }
+        let newMessage = document.createElement("li");
+        let chatBorder = document.createElement("div");
+        let UserprofileLine = document.createElement("div");
+        let Username = document.createElement("div");
+        let messageP = document.createElement("p");
+        chatBorder.classList.add("ChatBorder");
+        UserprofileLine.classList.add("UserProfileLine");
+        profilePicIMG.classList.add("ProfilePicture");
+        Username.classList.add("Username");
+        messageP.classList.add("Message");
+        Username.innerHTML = user + ":";
+        messageP.innerHTML = message;
+        UserprofileLine.append(profilePicIMG);
+        UserprofileLine.append(Username);
+        chatBorder.append(UserprofileLine);
+        chatBorder.append(messageP);
+        newMessage.append(chatBorder);
+        chat.append(newMessage);
+        if (chat.getElementsByTagName("li").length > ClearOldChatMSGsAfter) {
+            (_a = chat.firstElementChild) === null || _a === void 0 ? void 0 : _a.remove();
+        }
+    });
+}
+function validateToken() {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (AppAcessToken != undefined &&
+            AppAcessToken != "" &&
+            AppAcessToken != null) {
+            yield fetch("https://id.twitch.tv/oauth2/validate", {
+                headers: {
+                    Authorization: "Bearer " + AppAcessToken,
+                },
+            })
+                .then((resp) => resp.json())
+                .then((resp) => {
+                if (resp.status) {
+                    if (resp.status == 401) {
+                        console.log("This token is invalid ... " + resp.message);
+                        return 0;
+                    }
+                    console.log("Unexpected output with a status");
+                    return 0;
+                }
+                if (resp.client_id) {
+                    AclientId = resp.client_id;
+                    console.log("Token Validated Sucessfully");
+                    return 1;
+                }
+                console.log("unexpected Output");
+                return 0;
+            })
+                .catch((err) => {
+                console.log(err);
+                return 0;
+            });
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    });
+}
+function HttpCalling(HttpCall) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const respon = yield fetch(`${HttpCall}`, {
+            headers: {
+                Authorization: "Bearer " + AppAcessToken,
+                "Client-ID": AclientId,
+            },
+        })
+            .then((respon) => respon.json())
+            .then((respon) => {
+            return respon;
+        })
+            .catch((err) => {
+            console.log(err);
+            return err;
+        });
+        return respon;
+    });
 }
--- index.ts
+++ index.ts
@@ -3,8 +3,11 @@ var chat = document.querySelector("#chat>ul") as HTMLElement;
 
 //#region Basic Settings
 let ClearOldChatMSGsAfter = 11 as number;
-//var ComfyJS = require("https://cdn.jsdelivr.net/npm/comfy.js@latest/dist/comfy.min.js");
 
+
+let AppAcessToken = "gksx1g5gtd21dukq6t3cdafslng28t";
+let AclientId = "";
+validateToken();
 //#endregion
 
 //@ts-expect-error
@@ -39,21 +42,16 @@ ComfyJS.onCommand = (
   flags: any,
   extra: any
 ) => {
-  
   if (command === "lurk") {
     //@ts-expect-error
-    ComfyJS.Say(
-      "Have a nice lurk @" +
-        user +
-        "!! 🌺🌸"
-    );
+    ComfyJS.Say("Have a nice lurk @" + user + "!! 🌺🌸");
   }
-  if(flags.broadcaster || flags.mod) {
+  if (flags.broadcaster || flags.mod) {
     if (command === "test") {
       //@ts-expect-error
       ComfyJS.Say("replying to !test");
     }
-    if(command === "clear") {
+    if (command === "clear") {
       chat.innerHTML = "";
       //@ts-expect-error
       ComfyJS.Say("Cleared On-Screen Chatbox! 🧹🤖");
@@ -69,26 +67,142 @@ ComfyJS.onMessageDeleted = (id: any, extra: any) => {
 //@ts-expect-error
 ComfyJS.Init(
   "illu_illusion",
-  "",
+  "oauth:ittskpnutx42o6zlmps13yotc9zylg",
   "grat_grot10_berg"
 );
 
-function CreateChatText(
+// FUNCTIONS
+
+var ChatProfiles = Array();
+
+async function CreateChatText(
   message: string,
   user: string,
   colour: string,
   profilePic: string,
   emotes: Array<string>
 ) {
-  var newMessage = document.createElement("li");
-  var text = document.createElement("blockquote");
-  newMessage.innerText = user;
-  text.innerText = message;
-  newMessage.append(text);
+
+  // Getting Profile picture
+  let profilePicIMG = document.createElement("img") as HTMLImageElement;
+  if(ChatProfiles.indexOf(user) == -1) {
+    let User = await HttpCalling("https://api.twitch.tv/helix/users?login="+user);
+    console.log(User);
+    profilePicIMG.src=User["data"][0]["profile_image_url"];
+    ChatProfiles.push(User["data"][0]["login"]+"#"+User["data"][0]["profile_image_url"]);
+  }
+  else {
+    console.log(ChatProfiles);
+    profilePicIMG.src = ChatProfiles[ChatProfiles.indexOf(user)];
+  }
+
+  // Vars
+  let newMessage = document.createElement("li") as HTMLLIElement;
+  let chatBorder = document.createElement("div") as HTMLDivElement;
+  let UserprofileLine = document.createElement("div") as HTMLDivElement;
+  
+  let Username = document.createElement("div") as HTMLDivElement;
+  let messageP = document.createElement("p") as HTMLParagraphElement;
+
+  // Attributes
+  chatBorder.classList.add("ChatBorder");
+  UserprofileLine.classList.add("UserProfileLine");
+  profilePicIMG.classList.add("ProfilePicture");
+  Username.classList.add("Username");
+  messageP.classList.add("Message");
+
+  // Values
+  Username.innerHTML = user+":";
+  messageP.innerHTML = message;
+
+  // Appending
+  UserprofileLine.append(profilePicIMG);
+  UserprofileLine.append(Username);
+  chatBorder.append(UserprofileLine);
+  chatBorder.append(messageP);
+  newMessage.append(chatBorder);
   chat.append(newMessage);
+
+  //#region Legazy Chatembeding
+  // var newMessage = document.createElement("li");
+  // var text = document.createElement("blockquote");
+  // newMessage.innerText = user;
+  // text.innerText = message;
+  // newMessage.append(text);
+  // chat.append(newMessage);
+  //#endregion
 
   // removes old chat messages outside of view.
   if (chat.getElementsByTagName("li").length > ClearOldChatMSGsAfter) {
     chat.firstElementChild?.remove();
   }
 }
+
+
+
+//#region validateToken() Validates Token if sucessful returns 1 if not 0
+// Calls the Twitch api with Out App Acess Token and returns a ClientId and tells us if the App Acess Token is Valid or Not
+async function validateToken() {
+  if (
+    AppAcessToken != undefined &&
+    AppAcessToken != "" &&
+    AppAcessToken != null
+  ) {
+    await fetch("https://id.twitch.tv/oauth2/validate", {
+      headers: {
+        Authorization: "Bearer " + AppAcessToken,
+      },
+    })
+      .then((resp) => resp.json())
+      .then((resp) => {
+        if (resp.status) {
+          if (resp.status == 401) {
+            console.log("This token is invalid ... " + resp.message);
+            return 0;
+          }
+          console.log("Unexpected output with a status");
+          return 0;
+        }
+        if (resp.client_id) {
+          AclientId = resp.client_id;
+         
+          console.log("Token Validated Sucessfully");
+          return 1;
+        }
+        console.log("unexpected Output");
+        return 0;
+      })
+      .catch((err) => {
+        console.log(err);
+        return 0;
+      });
+    return 1;
+  } else {
+    return 0;
+  }
+}
+//#endregion
+
+//#region [async] HttpCaller(HttpCall) multipurpose HttpCaller calls the Httpcall returns The Response if Success if not: 0
+// This makes most calls, intead of a lot of differnt functions this does them instead.
+// TO find out what is called look where its called as the HTTPCALL would need to be sent over.
+async function HttpCalling(HttpCall: string) {
+  const respon = await fetch(`${HttpCall}`, {
+    headers: {
+      Authorization: "Bearer " + AppAcessToken,
+      "Client-ID": AclientId, // can also use Tclient_id. !! comment out Tclient if not being used !!
+    },
+  })
+    .then((respon) => respon.json())
+    .then((respon) => {
+      // Return Response on Success
+      return respon;
+    })
+    .catch((err) => {
+      // Print Error if any. And return 0
+      console.log(err);
+      return err;
+    });
+  return respon;
+}
+//#endregion
